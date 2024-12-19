6325c6cee2ff7d98cffaa00a51b02cf3d788e8da
yg.lee <yg.lee@quintet.co.kr>
2024-12-19 13:45:48 +0900

    Development Supabase/auth

--- src/main/handler/initHandler.ts
+++ src/main/handler/initHandler.ts
@@ -1,7 +1,21 @@
 import { ipcMain } from 'electron/main'
 import type { CoreBaseHandler } from '@base/CoreBaseHandler'
 
+/* Auth Handler */
+import { DeleteUserHandler } from './auth/DeleteUserHandler'
+import { SignOutHandler } from './auth/SignOutHandler'
+import { SignInWithOtpHandler } from './auth/SignInWithOtpHandler'
+import { VerifyOtpTokenHandler } from './auth/VerifyOtpTokenHandler'
+import { GetSessionHandler } from './auth/GetSessionHandler'
+
+
 export function initHandler() {
-  const handler: CoreBaseHandler[] = []
+  const handler: CoreBaseHandler[] = [
+    new DeleteUserHandler(),
+    new SignOutHandler(),
+    new SignInWithOtpHandler(),
+    new VerifyOtpTokenHandler(),
+    new GetSessionHandler()
+  ]
   handler.forEach(({ ipcChannel, handler }) => ipcMain.on(ipcChannel, handler))
 }
