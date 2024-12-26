5536b4f3a089cdeef86d1a72716ee7e476887b1d
jujoycode <jh.b.ryu@gmail.com>
2024-12-26 10:47:33 +0900

    🐌 change folder name (routes -> routers)

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,6 +1,6 @@
 import { RouterProvider } from 'react-router-dom'
 import { Toaster } from '@components/ui/toaster'
-import { router as HydraRouter } from '@routes'
+import { router as HydraRouter } from 'src/renderer/src/routers'
 
 function App(): JSX.Element {
   return (
