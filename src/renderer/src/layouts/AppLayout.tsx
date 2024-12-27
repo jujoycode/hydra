06e9d25ae3b0be14a0b5d2c9092b683ff851fdbe
jujoycode <jh.b.ryu@gmail.com>
2024-12-27 15:39:01 +0900

    :hammer: change file name

--- src/renderer/src/layouts/AppLayout.tsx
+++ src/renderer/src/layouts/AppLayout.tsx
@@ -1,11 +1,12 @@
 import { Outlet } from 'react-router-dom'
 import { Container } from '@chakra-ui/react'
-import { TopNavigation } from '@components/features/nav/TobNavigation'
+import { TopNavigation } from '@components/features/nav/TopNavigation'
 
 export function AppLayout(): JSX.Element {
   return (
     <Container p={0} minH='screen'>
       <TopNavigation />
+
       <Container mx='auto' px={4} pt={4}>
         <Outlet />
       </Container>
