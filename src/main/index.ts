9032ccf42d0f8410477032a82004eb95edd01e15
jujoycode <jh.b.ryu@gmail.com>
2025-01-02 13:14:00 +0900

    🐌 Log style 통일

--- src/main/index.ts
+++ src/main/index.ts
@@ -5,7 +5,7 @@ import { CoreUtil } from '@util/CoreUtil'
 import { initHandler } from '@handler/initHandler'
 
 function simpleLog(message: string) {
-  console.log(`[\x1b[38;2;128;0;32mSYSTEM\x1b[0m] [${new Date().toISOString()}] ${message}`)
+  console.log(`\x1b[38;2;128;0;32m[SYSTEM]\x1b[0m [${new Date().toISOString()}] ${message}`)
 }
 
 function createWindow() {
